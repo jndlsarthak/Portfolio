6c6fee06908d1eead3b8b594dd681fabc790d4e0
jndlsarthak <jndlsarthak@gmail.com>
2026-03-13 01:58:49 -0400

    Configure Next.js and GitHub Actions for Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
+
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+};
 
 export default nextConfig;
